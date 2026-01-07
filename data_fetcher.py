d102dfc4bcd4fe7097f4cdb62f8a7463ea2269fc
Thannuj <148995959+thannujyadav85@users.noreply.github.com>
2026-01-07 21:44:15 +0530

    Clarify excel and image directory paths with comments
    Added comments to clarify the purpose of the paths.

--- data_fetcher.py
+++ data_fetcher.py
@@ -99,8 +99,8 @@ def bulk_download_images(
     print(f"Failed: {failed}")
 if __name__ == "__main__":
     bulk_download_images(
-        excel_path="data/train(1).xlsx",
-        image_dir="data/images",
+        excel_path="data/train(1).xlsx", #train and test dataset are kept in a folder named data
+        image_dir="data/images", #images are collected in the folder data
         zoom=18,
         sleep_time=0.2,
         max_images=None
